b3e088d762cb25c8ab6cc513d63fe575c25fb3df
Mustafa Kan <79532179+mustafakan01@users.noreply.github.com>
2023-06-07 21:10:39 +0300

    camera

---
+++ Assets/Script/CameraFollow.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform cameraTarget;
+    public float sSpeed=10f;
+    public Vector3 dist;
+    public Transform lookTarget;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 dPos = cameraTarget.position + dist;
+        Vector3 sPos=Vector3.Lerp(transform.position, dPos, sSpeed*Time.deltaTime);
+        transform.position = sPos;  
+        transform.LookAt(lookTarget.position);
+    }
+}
